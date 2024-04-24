76f87c67b1f2e64083719299865b8b00150fa89b
Paulo Henrique <phqnrz@gmail.com>
2024-04-24 17:39:04 -0300

    ajustes reveal

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 // FUNCIONALIDADE DE REVELAR OS ATRIBUTOS DA PÁGINA
 window.revelar = ScrollReveal({ reset: true });
 
+
 // TOPO DO SITE (first section)
 revelar.reveal(".efeito-txt-topo", {
   duration: 2000, // sempre colocar segundos * 1000
@@ -13,11 +14,13 @@ revelar.reveal(".efeito-img-topo", {
   delay: 300
 });
 
+
 // TÍTULOS DA PÁGINA
 revelar.reveal(".titulo", {
   duration: 1500, // sempre colocar segundos * 1000
   distance: '4rem'
 });
+
 
 // ESPECIALIDADES 
 revelar.reveal(".box1", {
@@ -38,6 +41,7 @@ revelar.reveal(".box3", {
   delay: 1500
 });
 
+
 // SOBRE
 revelar.reveal(".reveal-img-sobre", {
   duration: 2000, // sempre colocar segundos * 1000
@@ -49,9 +53,65 @@ revelar.reveal(".reveal-img-sobre", {
 revelar.reveal(".reveal-txt-sobre", {
   duration: 2000, // sempre colocar segundos * 1000
   distance: '10rem',
-  delay: 500,
+  delay: 1000,
   origin: 'left'
 });
+
+
+// PORTFOLIO
+revelar.reveal(".port1", {
+  duration: 2000, // sempre colocar segundos * 1000
+  distance: '6rem',
+  delay: 500
+});
+
+revelar.reveal(".port2", {
+  duration: 2000, // sempre colocar segundos * 1000
+  distance: '6rem',
+  delay: 1000
+});
+
+revelar.reveal(".port3", {
+  duration: 2000, // sempre colocar segundos * 1000
+  distance: '6rem',
+  delay: 1500
+});
+
+
+// CONTATO
+revelar.reveal(".nome", {
+  duration: 2000, // sempre colocar segundos * 1000
+  distance: '6rem',
+  delay: 1500,
+  origin: 'left'
+});
+
+revelar.reveal(".email", {
+  duration: 2000, // sempre colocar segundos * 1000
+  distance: '6rem',
+  delay: 1500,
+  origin: 'right'
+});
+
+revelar.reveal(".telefone", {
+  duration: 2000, // sempre colocar segundos * 1000
+  distance: '6rem',
+  delay: 1500,
+  origin: 'left'
+});
+
+revelar.reveal(".mensagem", {
+  duration: 2000, // sempre colocar segundos * 1000
+  distance: '6rem',
+  delay: 1500
+});
+
+revelar.reveal(".btn-form", {
+  duration: 2000, // sempre colocar segundos * 1000
+  distance: '6rem',
+  delay: 1500
+});
+
 
 
 
