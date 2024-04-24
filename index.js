77faba1770182e310fe33f762b460003fbbd193a
Paulo Henrique <phqnrz@gmail.com>
2024-04-24 09:53:37 -0300

    ajustes menu mobile

--- index.js
+++ index.js
@@ -1,11 +1,22 @@
 var btnMenu = document.getElementById("btn-menu");
 var menuMobile = document.getElementById("menu-mobile");
+var overlay = document.getElementById("overlay-menu");
+var btnFechar = document.getElementById("btn-fechar");
+
 
 btnMenu.addEventListener("click", () => {
   menuMobile.classList.add("abrir-menu");
 });
 
+btnFechar.addEventListener("click", () => {
+  menuMobile.classList.remove("abrir-menu");
+});
 
-menu.addEventListener("click", () => {
+
+menuMobile.addEventListener("click", () => {
+  menuMobile.classList.remove("abrir-menu");
+});
+
+overlay.addEventListener("click", () => {
   menuMobile.classList.remove("abrir-menu");
 });
